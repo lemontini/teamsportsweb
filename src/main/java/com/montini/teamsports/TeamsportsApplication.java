bacb4e9934c0e90a92d6b75fac9bb6813a8b405c
montini <lemontini@gmail.com>
2019-11-11 07:11:19 +0200

    Updates

--- src/main/java/com/montini/teamsports/TeamsportsApplication.java
+++ src/main/java/com/montini/teamsports/TeamsportsApplication.java
@@ -3,13 +3,23 @@ package com.montini.teamsports;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @SpringBootApplication
 @EnableOAuth2Sso
-public class TeamsportsApplication {
+public class TeamsportsApplication extends WebSecurityConfigurerAdapter {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TeamsportsApplication.class, args);
+    public static void main(String[] args) {
+        SpringApplication.run(TeamsportsApplication.class, args);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+		// super.configure(http);
+		http.csrf().disable().antMatcher("/**").authorizeRequests()
+                .antMatchers("/", "/login**").permitAll()
+                .anyRequest().authenticated().and().logout()
+                .logoutSuccessUrl("/").permitAll();
 	}
-
 }
